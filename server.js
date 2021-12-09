e19b98ff175be920434054f0410e687a3d3c1716
Tsiegmund <Tyler2023411@live.ytech.edu>
2021-12-09 09:59:18 -0500

    Started Add/Remove points system
    Currently writes ID and Points to a JSON, but is unable to physically find and change any of the values

--- server.js
+++ server.js
@@ -1,68 +1,101 @@
 const express = require('express');
 const readline = require('readline');
 const fs = require('fs');
+const path = require('path');
 app = express()
 const hostname = '127.0.0.1'
+const ejs = require('ejs');
 app.use(express.urlencoded({ extended: true}));
 const port = 1337;
 
 
 app.set('view engine', 'ejs');
 
-var rawdata = fs.readFileSync(__dirname + '/userinfo.json');
+
+
 
 
 app.get('/', function(req, res) { //view and edit scores
-  var rawinfo = fs.readFileSync(__dirname + "/userinfo.json")
-  feed = JSON.parse(rawinfo)
-  res.render('index.ejs', { //depending on perms ^
-    username:uName,
-    password: passw
-  });
+  // let rawinfo = fs.readFileSync(__dirname + "/userinfo.json")
+  // let feed = JSON.parse(rawinfo)
+  // let text = feed.text
+  res.render('index.ejs');
 });
 
-app.post('/', function(req, res) {
-  const user = req.body.Uname
-  const pass = req.body.Pass
-  var login = {
-    uName: user,
-    passw: pass
+
+app.post('/login', function(req, res) {
+  let login = {
+    name: req.body.uname,
+    password: req.body.pass
   }
+  console.log(login)
 
-  if (login.username && login.password) {
-    var rawinfo = fs.readFileSync('./userinfo')
-    feed = JSON.parse(rawinfo)
-    console.log(feed)
-    comments = feed.comments
-    comments.push(login)
-    var feed = {
-      comments: comments
-    }
-    var feed = JSON.stringify(feed)
+  if (login.name && login.password) {
+      let rawinfo = fs.readFileSync(__dirname + '/userinfo.json')
+      let feed = JSON.parse(rawinfo)
+      feed.comments.push(login);
+      console.log(feed);
+      fs.writeFile(__dirname + '/userinfo.json', JSON.stringify(feed), 'utf8', function() {
+        console.log('wrote to file')
+        res.render('index.ejs');
 
-    fs.writeFile(__dirname + '/userinfo.json', feed, 'utf8', function() {
-      console.log('wrote to file')
-      console.log(feed)
-    })
+      })
   }
 })
 
-app.get('/access', function(req, res) {
-  res.render('access.ejs', {//list of users and their scores, also used for adding and removing students
-  });
-});
+//
+//
+//
+//
+//
+//
+//
+// app.get('/access', function(req, res) {
+//   res.render('access.ejs', {//list of users and their scores, also used for adding and removing students
+//   });
+// });
+//
+ app.get("/teacher", function(req, res) {
+   res.render("teacher.ejs", {
+     // addamt: {},
+     // subamt: {},
+     // starteramt: 100
+   })
+ })
 
-app.get("/teacher", function(req, res) {
-  res.render("teacher.ejs", {
 
-  })
-})
+ app.post('/access', function(req, res) {
+   res.render('access.ejs', {
+   });
+ });
+
+ app.get('/addStudent?', function (req, res){
+   res.render('addStudent', {
+   })
+ })
+
+ app.post('/addStudent', function (req, res) {
+     var studentInfo = {
+       studentID: req.body.studentID,
+       points: req.body.points
+     }
+     console.log(studentInfo)
+
+     if (studentInfo.studentID) {
+         let rawdata = fs.readFileSync(__dirname + '/studentInfo.json')
+         let parse = JSON.parse(rawdata)
+         parse.students.push(studentInfo);
+         console.log(parse);
+         fs.writeFile(__dirname + '/studentInfo.json', JSON.stringify(parse), 'utf8', function() {
+           console.log('student ID written with points')
+           res.render('addStudent.ejs');
+
+         })
+
+     }
+   })
 
 
-app.post('/access', function(req, res) {
-  res.render('access.ejs', {
-  });
-});
 
 
 app.listen(port, hostname, () => {
